ef85b6998efefff5a5a2ac066dcbde6d7c7fc4ae
hamishliu <hamish.mc.liu@gmail.com>
2022-09-26 21:24:26 +0900

    basic user greeting and then printing the users name and age in a string

--- python.py
+++ python.py
@@ -1,2 +1,7 @@
-#   This is my test file with changes
-print ("hello world")
+#   Greeting the user who is playing my game
+print ("Welcome to my first game")
+print ("-----------------")
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print("Hello", name, "you are", age, "years old")
